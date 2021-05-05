4cddcfa2a2bb89719429f6f8edf85064e1efda04
Rodrigo Bruno <rodrigo.b.bruno@oracle.com>
2021-05-05 16:37:06 +0100

    Fixing wrong address in python script.

--- socialNetwork/scripts/init_social_graph_register_1.py
+++ socialNetwork/scripts/init_social_graph_register_1.py
@@ -47,7 +47,7 @@ if __name__ == '__main__':
     nodes = getNodes(file)
     edges = getEdges(file)
 
-  addr = "http://127.0.0.1:8080"
+  addr = "http://nginx-thrift:8080"
   loop = asyncio.get_event_loop()
   future = asyncio.ensure_future(register(addr, nodes))
   loop.run_until_complete(future)
